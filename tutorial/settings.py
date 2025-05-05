84e6f600f287fb98e610423c4078993006fd4c69
xorazmiy1996 <muhammadibragimov439@gmail.com>
2025-05-05 14:44:00 +0500

    edit IP

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -28,7 +28,7 @@ DEBUG = False
 
 # ALLOWED_HOSTS = ['localhost','127.0.0.1']
 
-ALLOWED_HOSTS = ['35.225.17.227','test-uchun.uz','www.test-uchun.uz']
+ALLOWED_HOSTS = ['35.226.152.177']
 
 # Application definition
 
