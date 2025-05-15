57e3b42e335dc6ddc403c89a3ba608d4428ef171
xorazmiy1996 <muhammadibragimov439@gmail.com>
2025-05-15 14:23:19 +0500

    remove gevent

--- gunicorn_config.py
+++ gunicorn_config.py
@@ -2,7 +2,8 @@ import multiprocessing
 
 
 workers = multiprocessing.cpu_count() * 2 + 1 # 8 CPU uchun worker soni
-worker_class = 'gevent'  # Asinxron I/O uchun worker klassi
+threads = 30
+worker_class = 'gthread'  # Asinxron I/O uchun worker klassi
 worker_connections = 1000  # Har bir workerdagi ulanishlar soni
 bind = '0.0.0.0:8000' # IP manzil va port
 timeout = 30 # So'rovga javob berish vaqti
